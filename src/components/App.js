f8001cd3746350ca0bc224776323d9f6afc77db0
alisaitbilgi <alisaitbilgi@gmail.com>
2018-12-31 14:20:32 +0300

    Create first intercations

--- src/components/App.js
+++ src/components/App.js
@@ -5,27 +5,75 @@ import "../styles/main.css";
 class App extends Component {
   constructor(props){
     super(props);
-    this.state = {todoItem: "Create first redux flow..."};
+    this.state = {
+      todoText: "",
+      todoItems: []
+    };
+  }
+
+  addTodo() {
+    return () => {
+      if (this.state.todoText.trim() !== "") {
+        const newTodoItems = this.state.todoItems.set(this.state.todoText);
+
+        this.setState({
+          todoText: "",
+          todoItems: newTodoItems
+        })
+      }
+    }
+  }
+
+  removeTodo(index) {
+    return () => {
+      const newTodoItems = this.state.todoItems.filter((each, idx) => idx !== index);
+
+      this.setState({
+        todoText: "",
+        todoItems: newTodoItems
+      });
+    }
+  }
+
+  setInputText() {
+    return e => {
+      this.setState({
+        todoText: e.target.value
+      });
+    }
   }
 
   render() {
     return (
       <div className="app">
-        <form className="form-container">
-          <input className="input-field" placeholder={"Task Description"}/>
-          <button className="add-task-btn">
+        <form className="form-container" onSubmit={(e) => e.preventDefault()}>
+          <input
+            className="input-field"
+            placeholder={"Task Description"}
+            value={this.state.todoText}
+            onChange={this.setInputText()}
+          />
+          <button
+            onClick={this.addTodo()}
+            className="add-task-btn"
+          >
             {"Add Task"}
           </button>
         </form>
         <div className="list-container">
-          <div className="each-todo">
-            <span>{this.state.todoItem}</span>
-            <button
-              className="delete-icon"
-            >
-              {"X"}
-            </button>
-          </div>
+          {
+            this.state.todoItems.map((eachTodo, idx) => (
+              <div className="each-todo" key={idx}>
+                <span>{eachTodo}</span>
+                <button
+                  onClick={this.removeTodo(idx)}
+                  className="delete-icon"
+                >
+                  {"X"}
+                </button>
+              </div>
+            ))
+          }
         </div>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -2,4 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.js';
 
+Array.prototype.set = function (item) {
+  this.push(item);
+
+  return this;
+}
+
 ReactDOM.render(<App />, document.getElementById('root'));
