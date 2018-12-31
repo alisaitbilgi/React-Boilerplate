161c3eecd3c949f622bd01f131e35e1657c80d3b
alisaitbilgi <alisaitbilgi@gmail.com>
2018-12-31 15:09:59 +0300

    Create first redux flow

--- src/components/App.js
+++ src/components/App.js
@@ -9,7 +9,10 @@ class App extends Component {
     super(props);
     this.store = createStore(reducer);
     this.state = {
-      main: {}
+      main: {
+        currentText: "",
+        todoItems: []
+      }
     };
   }
 
@@ -21,27 +24,58 @@ class App extends Component {
     });
   }
 
+  setText() {
+    return e => {
+      const action = {
+        type: "SET_TEXT",
+        data: e.target.value
+      };
+
+      this.store.dispatch(action);
+    }
+  }
+
+  addTodo() {
+    return () => {
+      const action = {
+        type: "ADD_TODO",
+        data: this.state.main.currentText
+      }
+
+      this.store.dispatch(action);
+    }
+  }
+
   render() {
     return (
       <div className="app">
-        <form className="form-container">
+        <form className="form-container" onSubmit={e => e.preventDefault()}>
           <input
             className="input-field"
             placeholder={"Task Description"}
+            onChange={this.setText()}
+            value={this.state.main.currentText}
           />
-          <button className="add-task-btn">
+          <button
+            className="add-task-btn"
+            onClick={this.addTodo()}
+          >
             {"Add Task"}
           </button>
         </form>
         <div className="list-container">
-          <div className="each-todo">
-            <span>{this.state.todoItem}</span>
-            <button
-              className="delete-icon"
-            >
-              {"X"}
-            </button>
-          </div>
+          {
+            this.state.main.todoItems.map((eachTodo, idx) => (
+              <div className="each-todo" key={idx}>
+                <span>{eachTodo}</span>
+                <button
+                  className="delete-icon"
+                >
+                  {"X"}
+                </button>
+              </div>
+            ))
+          }
         </div>
       </div>
     );
--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,5 +1,19 @@
-function reducer(store = {}, action) {
+function reducer(
+  store = {
+    currentText: "",
+    todoItems: []
+  },
+  action
+) {
   switch (action.type) {
+
+    case "SET_TEXT":
+    store.currentText = action.data;
+    return store;
+
+    case "ADD_TODO":
+    store.todoItems.push(action.data);
+    return store;
 
     default:
       return store;
