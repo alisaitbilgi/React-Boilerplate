45e663c2e2cd730d8d6cca22857cf4755a10d47b
alisaitbilgi <alisaitbilgi@gmail.com>
2019-03-29 11:49:12 +0300

    Complete step-1

--- src/components/app.js
+++ src/components/app.js
@@ -5,21 +5,52 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      todoText: "Hello TEB",
+      todoText: "",
       todoItems: ["Do workshop", "Make presentation", "Say hello"]
     };
   }
 
+  handleChange() {
+    return e => {
+      this.setState({
+        todoText: e.target.value
+      })
+    }
+  }
+
+  handleClick() {
+    return () => {
+      const { todoItems, todoText } = this.state;
+      const newTodoItems = todoItems.set(todoText);
+
+      this.setState({
+        todoItems: newTodoItems
+      })
+    }
+  }
+
   render() {
+    const { todoItems, todoText } = this.state;
+
     return (
       <div className="app">
         <form className="form-container" onSubmit={(e) => e.preventDefault()}>
-          <input className="input-field"/>
-          <button className="add-task-btn">{"Add Task"}</button>
+          <input
+            className="input-field"
+            onChange={this.handleChange()}
+            value={todoText}
+          />
+          <button
+            className="add-task-btn"
+            onClick={this.handleClick()}
+          >
+            {"Add Task"}
+          </button>
         </form>
         <div className="list-container">
           {
-            this.state.todoItems.map((eachTodo, idx) => (
+            todoItems &&
+            todoItems.map((eachTodo, idx) => (
               <div className="each-todo" key={idx}>
                 <span>{eachTodo}</span>
                 <button className="delete-icon">{"X"}</button>
